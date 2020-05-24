03947957be1547d757a346a44b90afc33007f945
yrd <yash.deorah@somaiya.edu>
2020-05-24 21:56:01 +0530

    meri marzi3

--- bb/settings.py
+++ bb/settings.py
@@ -128,6 +128,7 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
